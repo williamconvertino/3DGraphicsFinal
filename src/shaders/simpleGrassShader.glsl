3243eb85ec60fbcd87675960bca93717b79913bf
William Convertino <williamconvertino@gmail.com>
2022-12-11 20:20:00 +0100

    Added really slow grass

---
+++ src/shaders/simpleGrassShader.glsl
@@ -0,0 +1,66 @@
+
+#ifdef VERTEX_SHADER
+// ------------------------------------------------------//
+// ----------------- VERTEX SHADER ----------------------//
+// ------------------------------------------------------//
+
+//Position of vertex
+attribute vec3 a_position;
+
+
+//Tranformation data
+uniform mat4 u_matrixM; 
+uniform mat4 u_matrixV; 
+uniform mat4 u_matrixP; 
+uniform mat3 u_matrixInvTransM;
+
+//Texture info
+attribute vec2 a_texcoord;
+varying vec2 v_texcoord;
+
+//Grass UV
+uniform vec2 u_uv;
+
+//Grass info
+uniform float u_height;
+uniform float u_grassBottom;
+uniform float u_waveDistance;
+uniform float u_time;
+
+
+void main() {
+    
+    //Texture setup
+    v_texcoord = a_texcoord;
+
+    vec3 finalPosition = a_position;
+    float relativeHeight = a_position.y - u_grassBottom;
+    float centerHeight = u_height / 2.0;
+    if (relativeHeight > centerHeight) {
+        finalPosition.x += sin((u_time / 500.0) + (u_uv.x * u_waveDistance)) * (relativeHeight - centerHeight);
+    }
+
+    //Calculate vertex position
+    gl_Position = u_matrixP * u_matrixV * u_matrixM * vec4 (finalPosition, 1);
+}
+
+#endif
+#ifdef FRAGMENT_SHADER
+// ------------------------------------------------------//
+// ----------------- Fragment SHADER --------------------//
+// ------------------------------------------------------//
+
+precision highp float; //float precision settings
+
+uniform vec3 u_tint;
+
+//Texture info
+uniform sampler2D u_mainTex;
+varying vec2 v_texcoord;
+
+void main(void){
+
+    gl_FragColor = vec4(texture2D(u_mainTex, v_texcoord).rgb, 1);
+}
+
+#endif
---
+++ src/simpleGrassRenderer.js
@@ -0,0 +1,49 @@
+"use strict";
+
+class SimpleGrassRenderer extends Renderer {
+
+    setVertexAttributeArrays(model){
+        super.setVertexAttributeArrays(model);
+
+        //Texture data
+        gl.bindBuffer(gl.ARRAY_BUFFER, model.mesh.texcoordBuffer);
+        let texcoordAttribLoc = gl.getAttribLocation(this.program, "a_texcoord");
+        gl.enableVertexAttribArray(texcoordAttribLoc);
+        gl.vertexAttribPointer(texcoordAttribLoc,2,gl.FLOAT,false,0,0);
+
+    }
+
+    /**
+    * Sets ALL uniforms for the vertex and fragment shader of this renderers shader program before drawing.
+    * @param {ModelTransform} model the model to draw.
+    * @param {Object} shaderData whatever other data the Shader needs for drawing.
+    */
+    setUniformData(model, camera, shaderData){
+        super.setUniformData(model, camera, shaderData);
+
+        //Texture data
+        gl.activeTexture(gl.TEXTURE0);
+        let mainTexture = TextureCache[model.material.mainTexture];
+        gl.bindTexture(gl.TEXTURE_2D, mainTexture);
+        let mainTextureLoc = gl.getUniformLocation(this.program, "u_mainTex");
+        gl.uniform1i(mainTextureLoc, 0);
+
+        //Grass data
+        let heightLoc = gl.getUniformLocation(this.program, "u_height");
+        gl.uniform1f(heightLoc, shaderData.height);
+
+        let grassBottomLoc = gl.getUniformLocation(this.program, "u_grassBottom");
+        gl.uniform1f(grassBottomLoc, shaderData.grassBottom);
+        
+        let waveDistanceLoc = gl.getUniformLocation(this.program, "u_waveDistance");
+        gl.uniform1f(waveDistanceLoc, shaderData.waveDistance);
+        
+        let timeLoc = gl.getUniformLocation(this.program, "u_time");
+        gl.uniform1f(timeLoc, shaderData.time);
+        
+        let uvLoc = gl.getUniformLocation(this.program, "u_uv");
+        gl.uniform2fv(uvLoc, shaderData.uv);
+        
+
+    }
+}
