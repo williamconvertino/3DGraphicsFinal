53e19dee6b9b7da8c8b1b1c3a22b502919fac432
William Convertino <williamconvertino@gmail.com>
2022-12-12 19:06:12 +0100

    Attempt 3

--- src/shaders/skyboxShader.glsl
+++ src/shaders/skyboxShader.glsl
@@ -5,23 +5,13 @@
 // ------------------------------------------------------//
 
 //Position of vertex
-attribute vec3 a_position;
-varying vec3 v_texCoord;
-
-uniform mat4 u_matrixV; 
-uniform mat4 u_matrixP;
-uniform mat4 u_matrixM;
-
-uniform mat4 u_invMatrixV; 
-uniform mat4 u_invMatrixP;
-uniform mat4 u_invMatrixM;
-
-uniform mat4 u_skyboxMatrix;
+attribute vec4 a_position;
+varying vec4 v_texCoord;
 
 void main() {
-    
-    v_texCoord = (vec4 (a_position, 1)).xyz;
-    gl_Position =  vec4(a_position, 1.0);
+    v_texCoord = a_position;
+    gl_Position = a_position;
+    gl_Position.z =  1.0;
 }
 
 #endif
@@ -33,11 +23,14 @@ void main() {
 precision highp float; //float precision settings
 
 
-varying vec3 v_texCoord;
+varying vec4 v_texCoord;
 uniform samplerCube u_skyboxTex;
 
+uniform mat4 u_skyboxMatrix;
+
 void main(){
-    gl_FragColor = textureCube(u_skyboxTex, v_texCoord);
+    vec4 t = u_skyboxMatrix * v_texCoord;
+    gl_FragColor = textureCube(u_skyboxTex, normalize(t.xyz / t.w));
 }
 
 #endif
--- src/skyboxRenderer.js
+++ src/skyboxRenderer.js
@@ -3,7 +3,25 @@
 class SkyboxRenderer extends Renderer {
 
     setVertexAttributeArrays(model){
-        super.setVertexAttributeArrays(model);
+        //super.setVertexAttributeArrays(model);
+        let skyboxCoords = new Float32Array(
+            [
+                -1, -1, 
+                1, -1, 
+                -1,  1, 
+                -1,  1,
+                1, -1,
+                1,  1,
+            ]);
+
+        let positionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, skyboxCoords, gl.STATIC_DRAW)
+
+        let posAttribLoc = gl.getAttribLocation(this.program, "a_position");
+        gl.enableVertexAttribArray(posAttribLoc);
+
+        gl.vertexAttribPointer(posAttribLoc,2,gl.FLOAT,false,0,0);
     }
 
     /**
@@ -16,22 +34,22 @@ class SkyboxRenderer extends Renderer {
         let viewMatrix = camera.viewMatrix;
         let projectionMatrix = camera.projectionMatrix;
 
-        // set model, view and projection matrices in the vertex shader
-        let modelMatrixLoc = gl.getUniformLocation(this.program, "u_matrixM");
-        gl.uniformMatrix4fv(modelMatrixLoc, false, model.modelMatrix.toFloat32());
-        let viewMatrixLoc = gl.getUniformLocation(this.program, "u_matrixV");
-        gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix.toFloat32());
-        let projMatrixLoc = gl.getUniformLocation(this.program, "u_matrixP");
-        gl.uniformMatrix4fv(projMatrixLoc, false, projectionMatrix.toFloat32());
+        // // set model, view and projection matrices in the vertex shader
+        // let modelMatrixLoc = gl.getUniformLocation(this.program, "u_matrixM");
+        // gl.uniformMatrix4fv(modelMatrixLoc, false, model.modelMatrix.toFloat32());
+        // let viewMatrixLoc = gl.getUniformLocation(this.program, "u_matrixV");
+        // gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix.toFloat32());
+        // let projMatrixLoc = gl.getUniformLocation(this.program, "u_matrixP");
+        // gl.uniformMatrix4fv(projMatrixLoc, false, projectionMatrix.toFloat32());
 
         
-        // set model, view and projection matrices in the vertex shader
-        let modelMatrixLocInv = gl.getUniformLocation(this.program, "u_invMatrixM");
-        gl.uniformMatrix4fv(modelMatrixLocInv, false, M4.invert(model.modelMatrix).toFloat32());
-        let viewMatrixLocInv = gl.getUniformLocation(this.program, "u_invMatrixV");
-        gl.uniformMatrix4fv(viewMatrixLocInv, false, M4.invert(viewMatrix).toFloat32());
-        let projMatrixLocInv = gl.getUniformLocation(this.program, "u_invMatrixP");
-        gl.uniformMatrix4fv(projMatrixLocInv, false, M4.invert(projectionMatrix).toFloat32());
+        // // set model, view and projection matrices in the vertex shader
+        // let modelMatrixLocInv = gl.getUniformLocation(this.program, "u_invMatrixM");
+        // gl.uniformMatrix4fv(modelMatrixLocInv, false, M4.invert(model.modelMatrix).toFloat32());
+        // let viewMatrixLocInv = gl.getUniformLocation(this.program, "u_invMatrixV");
+        // gl.uniformMatrix4fv(viewMatrixLocInv, false, M4.invert(viewMatrix).toFloat32());
+        // let projMatrixLocInv = gl.getUniformLocation(this.program, "u_invMatrixP");
+        // gl.uniformMatrix4fv(projMatrixLocInv, false, M4.invert(projectionMatrix).toFloat32());
         
         let skyboxMatrixLoc = gl.getUniformLocation(this.program, "u_skyboxMatrix");
         gl.uniformMatrix4fv(skyboxMatrixLoc, false, M4.invert(M4.multM4( projectionMatrix, viewMatrix)).toFloat32());
@@ -46,7 +64,23 @@ class SkyboxRenderer extends Renderer {
     }
     drawModel(model, camera, shaderData){
         gl.disable(gl.DEPTH_TEST);
-        super.drawModel(model, camera, shaderData);
+        //super.drawModel(model, camera, shaderData);
+        
+        
+        gl.useProgram(this.program);
+
+        // set the arrtibute arrays and uniform data for this programs vertex and
+        // fragment shader based on the models buffer data and material
+        this.setVertexAttributeArrays(model);
+        this.setUniformData(model, camera, shaderData);
+
+        // draw call using index based triangle assembly (elements)
+        //gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.mesh.bufIndex);
+        //gl.drawElements(model.mesh.drawMode, model.mesh.indexCount, gl.UNSIGNED_SHORT, 0);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 1 * 6);
+        
         gl.enable(gl.DEPTH_TEST);
+        return this;
     }
 }
